868030a8a3c258da7336f8efae4649ddfcc088d1
Vladislav Shevchenko <shev.vlad1991@gmail.com>
2020-09-28 17:10:09 +0300

    fixes at htmlcomponent

--- src/components/HtmlComponent/HtmlComponent.tsx
+++ src/components/HtmlComponent/HtmlComponent.tsx
@@ -3,10 +3,10 @@ import styles from './HtmlComponent.module.scss'
 import classNames from 'classnames'
 import { PropsType } from './types';
 
-const HtmlComponent: React.FC<PropsType> = ({ title, component, variant, className, children }) => {
-    const CustomTag = variant;
+const HtmlComponent: React.FC<PropsType> = ({ title, component: Component = 'div', variant, className, children }) => {
+
     return (
-        <CustomTag className={classNames(styles.root, className,
+        <Component className={classNames(styles.root, className,
             {
                 [styles.h1]: variant === 'h1',
                 [styles.h2]: variant === 'h2',
@@ -15,7 +15,7 @@ const HtmlComponent: React.FC<PropsType> = ({ title, component, variant, classNa
 
             })}>
             {title}{children}
-        </CustomTag>
+        </Component>
     )
 }
 
--- src/components/HtmlComponent/types.ts
+++ src/components/HtmlComponent/types.ts
@@ -3,5 +3,5 @@ export interface PropsType extends React.HTMLAttributes<HTMLHeadingElement> {
     title?: string,
     variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6",
     className?: string,
-    component?: React.ReactNode
+    component?: React.ElementType
 }
