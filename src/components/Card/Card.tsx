4f57d438eeb1507af95e9e96a8cd4ad5a1352908
Vladislav Shevchenko <shev.vlad1991@gmail.com>
2020-09-28 20:24:24 +0300

    add material-ui and rewrite card and homePage(grid)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { PropsType } from './types';
-import styles from './Card.module.scss'
-import HtmlComponent from 'components/HtmlComponent';
+// import HtmlComponent from 'components/HtmlComponent';
 import classNames from 'classnames'
+import { CardActionArea, CardMedia, Card as CustomCard, CardContent, Typography } from '@material-ui/core';
+import styles from './Card.module.scss'
 
 const Card: React.FC<PropsType> = ({ img, title, text, small }) => {
     return (
-        <div className={styles.root}>
-            <div className={classNames(styles.img, { [styles.sm]: small })}>
-                <img src={img} alt={`img-${title}`} />
-            </div>
-            <div className={styles.body}>
-                <HtmlComponent component='h4' variant='h4' className={styles.title}>{title}</HtmlComponent>
-                <div className={styles.text}>
-                    {text}
+        // <div className={styles.root}>
+        //     <div className={classNames(styles.img, { [styles.sm]: small })}>
+        //         <img src={img} alt={`img-${title}`} />
+        //     </div>
+        //     <div className={styles.body}>
+        //         <HtmlComponent component='h4' variant='h4' className={styles.title}>{title}</HtmlComponent>
+        //         <div className={styles.text}>
+        //             {text}
+        //         </div>
+        //     </div>
+        // </div>
+
+        <CustomCard className={styles.root}>
+            <CardActionArea>
+                {/* <CardMedia
+                    className={classNames(styles.img, { [styles.sm]: small })}
+                    image={img}
+                    title={`img-${title}`}
+                /> */}
+                <div className={classNames(styles.img, { [styles.sm]: small })}>
+                    <img src={img} alt={`img-${title}`} />
                 </div>
-            </div>
-        </div>
+                <CardContent>
+                    <Typography gutterBottom variant="h4" component="h4" className={styles.title}>
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" className={styles.text} component="p">
+                        {text}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </CustomCard>
     )
 }
 
--- src/features/HomePage/HomePage.tsx
+++ src/features/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import FirstScreen from './components/FirstScreen'
 import styles from './HomePage.module.scss'
-import { Col, Row } from 'react-bootstrap';
 import NewSeason from './components/NewSeason';
 import NewColaction from './components/NewColaction';
 import Arrivals from './components/Arrivals';
 import Brands from './components/Brands';
 import { items } from './constants';
-
+import Grid from '@material-ui/core/Grid';
+import { Box } from '@material-ui/core';
 
 
 const HomePage: React.FC = () => {
@@ -15,23 +15,28 @@ const HomePage: React.FC = () => {
     return (
         <>
             <FirstScreen />
-            <Row className='m-0'>
-                <Col lg={12} xl={7} className='p-0'>
+            <Grid container>
+                <Grid item lg={7} xs={12}>
                     <NewSeason />
-                </Col>
-                <Col lg={12} xl={5} className='p-0'>
+                </Grid>
+                <Grid item lg={5} xs={12} >
                     <NewColaction title='Новая коллекция' btnTitle='Каталог' />
-                </Col>
-            </Row>
-            <Row className='m-0'>
-                <Col lg={12} xl={5} className='p-0 order-2 order-xl-1'>
-                    <NewColaction className={styles.bg} />
-                </Col>
-                <Col lg={12} xl={7} className='p-0 order-1 order-xl-2'>
-                    <NewColaction className={styles.nobg} title='коллекция 2018' btnTitle='посмотреть коллекцию'
-                        text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non rutrum ornare ut mattis habitant dui arcu. Sagittis amet nunc ut neque quis nibh arcu. Vivamus vestibulum nisi et venenatis sed scelerisque magna consectetur. Amet convallis quis gravida facilisis vulputate. Faucibus facilisi habitasse ipsum interdum dictum aliquet. Velit quis ullamcorper pulvinar nulla malesuada integer. Aenean praesent viverra nulla nullam natoque volutpat curabitur auctor. Viverra viverra ullamcorper scelerisque risus dignissim egestas. Id aliquam a aliquam egestas leo orci pharetra sed diam. ' />
-                </Col>
-            </Row>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Box clone order={{ xs: 2, lg: 1 }}>
+                    <Grid item lg={5} xs={12}>
+                        <NewColaction className={styles.bg} />
+                    </Grid>
+                </Box>
+                <Box clone order={{ xs: 1, lg: 2 }}>
+                    <Grid item lg={7} xs={12} >
+                        <NewColaction className={styles.nobg} title='коллекция 2018' btnTitle='посмотреть коллекцию'
+                            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non rutrum ornare ut mattis habitant dui arcu. Sagittis amet nunc ut neque quis nibh arcu. Vivamus vestibulum nisi et venenatis sed scelerisque magna consectetur. Amet convallis quis gravida facilisis vulputate. Faucibus facilisi habitasse ipsum interdum dictum aliquet. Velit quis ullamcorper pulvinar nulla malesuada integer. Aenean praesent viverra nulla nullam natoque volutpat curabitur auctor. Viverra viverra ullamcorper scelerisque risus dignissim egestas. Id aliquam a aliquam egestas leo orci pharetra sed diam. ' />
+
+                    </Grid>
+                </Box>
+            </Grid>
             <Arrivals items={items} />
             <Brands />
         </>
